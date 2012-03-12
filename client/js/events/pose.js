3b3092117517b5f3005bd873c4ddfe17d5191f2a
Accipiter Nisus <accipiter@gmail.com>
2012-03-12 21:58:37 +0100

    Added Whisper and Page capturing.

--- client/js/events/pose.js
+++ client/js/events/pose.js
@@ -4,24 +4,65 @@ poseEvent.prototype.callback = function($p) {
 
 	var text = $.trim($p.text());
 	var m;
-	if( m = text.match(/^([^ ]+)(.*)/) ) {
+	var mode = false;
+	var pre = '';
+	var mid = '';
+	var pos = '';
+	
+	if( m = text.match(/In a page-pose to you, ([^ ,']+)(.*)$/) ) {
+		mode = ' page';
+		pre = '(Page) ';
+	}
+	else if( m = text.match(/^([^ ]+) pages, "(.*)" to you.$/) ) {
+		mode = ' page';
+		mid = ' pages, "';
+		post = '" to you.';
+	}
+	else if( m = text.match(/^([^ ]+) whispers, "(.*)" to you.$/) ) {
 		
+		mode = ' whisper';
+		
+		if( m[2].substr(0, m[1].length) == m[1] && m[2].substr(m[1].length,1).match(/[ ',]/) ) {
+			pre = '(Whisper) ';
+			m[2] = m[2].substr(m[1].length);
+			post = '';
+		}
+		else
+		{
+			mid = ' whispers, "';
+			post = '" to you.';
+		}
+	}
+	else if( m = text.match(/^([^ ,']+)(.*)$/) ) {
+		mode = '';
+		Socket.send('ws ' + m[1]);
+		Event.append( new wsEvent({single:true}) );
+	}
+	
+	if( mode != false ) {
 		var lc = m[1].toLowerCase();
 				
 		var $span = $(document.createElement('span'))
 			.text(m[1])
 			.addClass('name');
 			
+		if( typeof Data.focus[lc] != 'undefined' ) {
+			var i = Data.focus[lc];
+			var cl = 'focus';
 		
-		if( Data.focus.indexOf(lc) != -1 )
-			$p.addClass('focus');
+			if( i >= 0 ) {
+				cl += ' ' + _focusColors[i];
+			}
+			
+			$p.addClass(cl);
+		}
 		
-		$p.html($span);
-		$p.append( document.createTextNode(m[2]) );
-		$p.addClass('char_'+lc);
-		
-		Socket.send('ws ' + m[1]);
-		Event.append( new wsEvent({single:true}) );		
+		$p
+			.text(pre)
+			.append($span)
+			.append( mid+m[2]+post )
+			.addClass('char_'+lc+mode);	
+				
 	}
 	
 	Log.add($p);
